4659b7ff02d342ce08406f93f015d6720bdc5938
Seunghyum <antmgu@gmail.com>
2019-06-15 15:04:38 +0900

    번들러 변경 webpack -> parcel

--- frontend/src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Header extends Component {
-    render() {
-        return (
-            <h1>This is Hdddddddeader!!!!!!ddddddd</h1>
-        )
-    }
-}
