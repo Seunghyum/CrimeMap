dbb66514a56e16559652f04512ef84c9f11f43e2
Seunghyum <antmgu@gmail.com>
2019-06-30 17:15:02 +0900

    geojson map frontend code setting

--- frontend/src/components/NaverMap.tsx
+++ frontend/src/components/NaverMap.tsx
@@ -1,6 +1,7 @@
 // import naver from "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=l98mpxcbdb";
 import React, { Component } from "react";
 import reactAsyncScript from "react-async-script";
+import sidoGeojson from '../../sido_2009.json';
 
 const ClientId: string = "l98mpxcbdb";
 const URL: string = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${ClientId}`;
@@ -15,19 +16,68 @@ class NaverMap extends Component<Props> {
   constructor(props: any) {
     super(props);
   }
-
-  scriptOnLoad() {
+  
+  naverMapscriptOnLoad() {
     const naver = window.naver;
     const center = new naver.maps.LatLng(36.0095704, 127.705399);
     const zoom = 2;
-    const map = new naver.maps.Map("map", {
+    let map = new naver.maps.Map("map", {
       center,
       zoom
     });
+
+    sidoGeojson.forEach((geojson) => {
+      map.data.addGeoJson(geojson);
+    });
+
+    map.data.setStyle((feature:any) => {
+        let styleOptions:any = {
+            fillColor: '#e5f5f9',
+            fillOpacity: 0.0001,
+            strokeColor: '#99d8c9',
+            strokeWeight: 2,
+            strokeOpacity: 0.4
+        };
+
+        if (feature.getProperty('focus')) {
+            styleOptions.fillOpacity = 0.6;
+            styleOptions.fillColor = '#0f0';
+            styleOptions.strokeColor = '#0f0';
+            styleOptions.strokeWeight = 4;
+            styleOptions.strokeOpacity = 1;
+        }
+
+        return styleOptions;
+    });
+
+    map.data.addListener('click', (e) => {
+      const feature:any = e.feature;
+
+      if (feature.getProperty('focus') !== true) {
+          feature.setProperty('focus', true);
+      } else {
+          feature.setProperty('focus', false);
+      }
+    });
+
+    map.data.addListener('mouseover', (e) => {
+      const feature:any = e.feature;
+
+      map.data.overrideStyle(feature, {
+        fillOpacity: 0.6,
+        strokeWeight: 4,
+        strokeOpacity: 1
+      });
+    });
+
+    map.data.addListener('mouseout', (e) => {
+      map.data.revertStyle();
+    });
+
   }
 
   render() {
-    return <NaverMapLoad asyncScriptOnLoad={this.scriptOnLoad} />;
+    return <NaverMapLoad asyncScriptOnLoad={this.naverMapscriptOnLoad} />;
   }
 }
 export default NaverMap;
