ce4a290e4d3233072bdfb0e76663607e3d202a2b
DongHyunKims <eldk1229@gmail.com>
2019-07-10 02:27:31 +0900

    테스크 작업중

---
+++ task/Gulpfile.babel.js
@@ -0,0 +1,7 @@
+const { src } = require('gulp');
+
+function defaultTask(cb) {
+  cb();
+}
+
+exports.default = defaultTask;
---
+++ task/src/crime.js
@@ -0,0 +1,9 @@
+const { series, parallel, src  } = require('gulp');
+const fs = require('fs');
+
+exports.default = {
+    'set-crime-data': async () => {
+        console.log('범죄 데이터를 입력한다.');
+
+    }
+};
